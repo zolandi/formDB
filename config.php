7830997283fc4fd2e4491a167a94e2641acb64ab
Felipe Enzo <58190413+zolandi@users.noreply.github.com>
2023-11-30 22:10:04 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername = 'root';
+    $dbPassword = '';
+    $dbName = 'formulario-projeto';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+
+ //   if ($conexao->connect_error) {
+  //      echo'erro';
+  //  }else{
+  //      echo'sucesso';
+ //   }
+
+
+?>
